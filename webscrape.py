baa08cad1ae6482ab6f0ec69842a238643c23751
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-23 01:48:17 -0400

    Learning scraping with python

--- webscrape.py
+++ webscrape.py
@@ -1,7 +1,25 @@
-from lib2to3.pgen2 import driver
-import pandas as pd 
-from bs4 import BeautifulSoup
-from selenium import webdriver
+# import requests
+# req = requests.request('GET', 'https://itsmycode.com/importerror-no-module-named-requests/')
+# print(req)
 
-driver = webdriver.Chrome(executable_path='/Users/maxmorgenbesser/Downloads/chromedriver/ ')
-driver.get('https://osxdaily.com/2015/11/05/copy-file-path-name-text-mac-os-x-finder/')
+
+#imports library which contains a open url method
+from urllib.request import urlopen
+#sets the url equal to something
+url = "https://itsmycode.com/importerror-no-module-named-requests/"
+#opens the page
+page = urlopen(url)
+
+# prints the html of the page so a person knows what to look for i
+#could also inspect
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+# print(html)
+
+# string method
+#finds the html index of the title of the url
+title_index=html.find("<title>")
+end_index = html.find("</title>")
+title=title = html[title_index:end_index]
+print(title)
+
